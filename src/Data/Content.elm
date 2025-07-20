7ade41960caccad93b4ef3c7b5315c8d1c6a23f7
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2025-07-20 21:29:11 +0200

    hugo rewrite

--- src/Data/Content.elm
+++
@@ -1,8 +0,0 @@
-module Data.Content exposing (mainContent)
-
-import Html exposing (Html, div, header, p, text)
-
-
-mainContent : Html msg
-mainContent =
-    div [] [ text "I am from Another File and this is actually the beginning of dynamic content loading" ]
